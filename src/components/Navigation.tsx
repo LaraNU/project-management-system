3bf0a75e21e38c8b945424764d7be4a9657d9cdb
Larisa Novozhilova <kara4516@gmail.com>
2025-06-04 21:24:23 +0300

    refactor: use new functions for menu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,16 +1,33 @@
 import { Button, Menu } from 'antd';
-import { Link, useLocation } from 'react-router-dom';
+import type { MenuProps } from 'antd';
+import { Link, useLocation, type Location } from 'react-router-dom';
 import styles from './Navigation.module.css';
 import logo from '../assets/logo.svg';
 
-export const Navigation = () => {
-  const location = useLocation();
+type MenuItem = Required<MenuProps>['items'][number];
 
-  const selectedKey = location.pathname.startsWith('/issues')
-    ? 'issues'
-    : location.pathname.startsWith('/boards')
-      ? 'boards'
-      : '';
+const items: MenuItem[] = [
+  {
+    label: <Link to="/issues">Все задачи</Link>,
+    key: 'issues',
+  },
+  {
+    label: <Link to="/boards">Проекты</Link>,
+    key: 'boards',
+  },
+];
+
+export const Navigation = () => {
+  const location: Location = useLocation();
+
+  const getSelectedKey = (): string => {
+    const { pathname } = location;
+
+    if (pathname.startsWith('/issues')) return 'issues';
+    if (pathname.startsWith('/boards')) return 'boards';
+
+    return '';
+  };
 
   return (
     <header className={styles.header}>
@@ -19,14 +36,12 @@ export const Navigation = () => {
           <img src={logo} alt="Logo" className={styles.logo} />
         </Link>
         <nav className={styles.nav}>
-          <Menu mode="horizontal" selectedKeys={[selectedKey]} className={styles.menu}>
-            <Menu.Item key="issues">
-              <Link to="/issues">Все задачи</Link>
-            </Menu.Item>
-            <Menu.Item key="boards">
-              <Link to="/boards">Проекты</Link>
-            </Menu.Item>
-          </Menu>
+          <Menu
+            mode="horizontal"
+            selectedKeys={[getSelectedKey()]}
+            items={items}
+            className={styles.menu}
+          />
         </nav>
       </div>
       <Button type="primary" style={{ fontSize: 'inherit' }}>
