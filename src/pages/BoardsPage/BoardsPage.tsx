b25bf6d4da8ce57b790e51ad1e096ca1d2698b62
Larisa Novozhilova <kara4516@gmail.com>
2025-06-05 16:21:06 +0300

    feat: uploud all boards on page

--- src/pages/BoardsPage/BoardsPage.tsx
+++ src/pages/BoardsPage/BoardsPage.tsx
@@ -1,11 +1,42 @@
 import styles from './BoardsPage.module.css';
+import { Spin, Divider } from 'antd';
+import { observer } from 'mobx-react-lite';
+import { boardStore } from '../../stores/BoardStore';
+import type { Board } from '../../types';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
-export const BoardsPage = () => {
+export const BoardsPage = observer(() => {
+  const { boards, loading } = boardStore;
+
+  useEffect(() => {
+    if (boards.length > 0) return;
+    boardStore.fetchBoards();
+  });
+
   return (
     <div className={styles.container}>
-      <h1>Boards Page</h1>
-      <Link to="/boards/1">Проект 1</Link>
+      <div className={styles.boardsContainer}>
+        <h2 className={styles.title}>All Boards</h2>
+        <Divider />
+        {loading ? (
+          <Spin size="large" />
+        ) : (
+          <ul className={styles.list}>
+            {boards.map((board: Board) => (
+              <li key={board.id} className={styles.boardItem}>
+                <div className={styles.boardInfo}>
+                  <h4 className={styles.boardTitle}>{board.name}</h4>
+                  <p className={styles.boardDescription}>{board.description}</p>
+                </div>
+                <Link to={`/boards/${board.id}`} role="button">
+                  Перейти к доске
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
-};
+});
